e1508f09bddb184c632ef33038cdeee5eb4abcaf
SalithaUCSC <salithachathuranga94@gmail.com>
2020-05-17 20:32:19 +0530

    add findMaxOfThree function

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 module.exports = {
     findMaxOfTwo: (x, y) => {
         let max;
-        if (typeof x !== Number || typeof y !== Number) {
+        if (typeof x !== "number" || typeof y !== "number") {
             throw new Error("Incompatible types: Arguments contains non numeric values");
         }
         else {
             if (x > y) {
                 max = x;
+            }
+            else {
+                max = y;
+            }
+            return max;
+        }
+    },
+    findMaxOfThree: (x, y, z) => {
+        let max;
+        if (typeof x !== "number" || typeof y !== "number" || typeof z !== "number") {
+            throw new Error("Incompatible types: Arguments contains non numeric values");
+        }
+        else {
+            if (x > y && x > z) {
+                max = x;
+            }
+            else if (x > y && x < z){
+                max = z;
             }
             else {
                 max = y;
